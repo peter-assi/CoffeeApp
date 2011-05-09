5bac3baedcc5972351fb3e8cd87460d98a8cca68
peterlind <peter@lind.be>
2011-05-09 10:08:38 +0200

    Empty Rails 3.1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoffeeApp::Application.config.secret_token = '9e2bf634f861d6325795f17d080417bfe5e97f6c3701858eb985271ac34c577d290a3c6373424e7a91260393f05e4277dac0eba1cd2d11cfaeff291e92512980'
